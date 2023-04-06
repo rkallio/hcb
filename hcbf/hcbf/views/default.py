08d51dcb1983ec5df1246d14c1405d5fd81080b6
Roni Kallio <roni.jj.kallio@gmail.com>
2023-04-06 13:12:56 +0300

    Add pagination functionality and update database connection string
    The commit includes adding pagination functionality to the default
    view and updating the database connection string to point to the
    correct file location. It also includes updating the template to
    display journey data in a table format.

--- hcbf/hcbf/views/default.py
+++ hcbf/hcbf/views/default.py
@@ -3,4 +3,14 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='hcbf:templates/mytemplate.jinja2')
 def my_view(request):
-    return {'project': 'hcbf'}
+    page = request.params.get('page', '1')
+    page = int(page)
+    res = request.db().execute("""
+    SELECT * FROM journey LIMIT 50 OFFSET ?""", [page * 50])
+    journeys = res.fetchall()
+    return {
+        'journeys': journeys,
+        'previous_page': request.current_route_path(_query={'page': max(0, page - 1)}),
+        'current_page_number': page,
+        'next_page': request.current_route_path(_query={'page': page + 1 })
+    }
